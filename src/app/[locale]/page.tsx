10b37aeb4eeb56b509015784fc1d0b719188c6d5
KhuongNQ <khuongnqdev@gmail.com>
2024-04-19 18:16:06 +0700

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,22 @@
 import HomeIndex from '../components/home';
+import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth/server';
+import { runWithAmplifyServerContext } from '@/app/utils/amplifyServerUtils';
+import { cookies } from 'next/headers';
 
 export default async function Home() {
+  const currentUser = await runWithAmplifyServerContext({
+    nextServerContext: { cookies },
+    operation: async (contextSpec) => {
+      try {
+        const session = await fetchAuthSession(contextSpec);
+        console.log(session)
+        return session.tokens !== undefined;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  });
   return (
     <HomeIndex />
   );
--- src/app/components/home/index.tsx
+++ src/app/components/home/index.tsx
@@ -1,12 +1,41 @@
-import { auth } from '@/auth';
-import { getServerSession } from 'next-auth';
+// import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/api';
+// import { cookies } from 'next/headers';
+'use client';
+import { generateClient } from 'aws-amplify/api';
+import { createBlog } from '@/graphql/mutations';
+import { Button, Form, Input } from 'antd';
 
-const HomeIndex = async () => {
-  const session = await auth();
-  
+
+const HomeIndex = () => {
+  const client = generateClient();
+  const createBlogFnc = async (formData: any) => {
+    console.log(formData)
+    const result = await client.graphql({
+      query: createBlog,
+      variables: {
+        input: {
+          name: formData.name,
+        },
+      },
+    })
+    console.log(result)
+  }
   return (
     <div>
-      <p>Hello User {session?.user?.email}</p>
+      <Form onFinish={createBlogFnc}>
+        <Form.Item
+          label="Blog Name"
+          name="name"
+          rules={[{ required: true, message: 'Please input blog name!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
